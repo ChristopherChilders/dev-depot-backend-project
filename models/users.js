b846da3518aff1e095d53e36cd21dbc2d425b399
Jason Boerner <jboerner56@gamil.com>
2019-04-10 11:29:45 -0400

    adds getById to user class

--- models/users.js
+++ models/users.js
@@ -23,4 +23,23 @@ class Users {
     static delete(id){
         return db.result('delete from users where id=$1', [id]);
     }
+    static getAll() {
+        
+    }
+    static getById(id){
+        return db.one(`select * from users where id=${id}`)
+        .then((userData) => {
+            const userInstance = new User(
+                userData.id,
+                userData.first_name,
+                userData.last_name,
+                userData.email,
+                userData.username,
+                userData.password
+            );
+        })
+        .catch(()=>{
+            return null;
+        })
+    }
 }
