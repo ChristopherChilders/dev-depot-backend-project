209f1030857845df89e8e710d871d4bbf1e725a2
Jason Boerner <jboerner56@gamil.com>
2019-04-10 15:12:58 -0400

    adds delete/retrieve all users to crud.js

--- controllers/crud.js
+++ controllers/crud.js
@@ -4,7 +4,19 @@ async function addUser (req, res) {
     const addUser = await User.add(req.body);
     res.json(`The added user was given the id ${addUser}`);
 }
+async function deleteUser (req, res) {
+    const { id } = req.params;
+    await User.delete(id);
+    console.log(`user with id: ${id} was deleted`);
+}
+async function retrieveAllUsers (req, res) {
+    const allUsers = await User.getAll();
+    console.log(allUsers);
+    res.json(allUsers);
+}
 
 module.exports = {
-    addUser
+    addUser,
+    deleteUser,
+    retrieveAllUsers
 };
