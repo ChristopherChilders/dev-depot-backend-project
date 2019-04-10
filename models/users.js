3ed1be70b27064d6f2efd2b9607ad29c32cef6fd
Jason Boerner <jboerner56@gamil.com>
2019-04-10 11:11:59 -0400

    adds delete/add user to user class

---
+++ models/users.js
@@ -0,0 +1,26 @@
+const db = require('./conn');
+const bcrypt = require('bcryptjs');
+
+class Users {
+    constructor(id, first_name, last_name, email, username, password) {
+        this.id = id;
+        this.firstName = first_name;
+        this.lastName = last_name;
+        this.email = email;
+        this.username = username;
+        this.password =password;
+    }
+    static add(userData){
+        return db.one(`
+        insert into users
+        (first_name, last_name, email, username, password)
+        values
+        ($1, $2, $3, $4, $5)`[first_name, last_name, email, username, password])
+        .then((data) => {
+            return data.id;
+        })
+    }
+    static delete(id){
+        return db.result('delete from users where id=$1', [id]);
+    }
+}
