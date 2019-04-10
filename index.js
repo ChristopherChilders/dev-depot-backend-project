339db3b080fce9e17e193b2c61dee301abd1dc00
Ryan Yim <yknyim@gmail.com>
2019-04-10 15:35:38 -0400

    allows users to log in as well as add crud function

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const app = express();
 const PORT = process.env.PORT;
 const es6Renderer = require('express-es6-template-engine');
 
+const User = require('./models/users');
+
 const session = require('express-session');
 const FileStore = require('session-file-store')(session);
 // const setupAuth = require('./auth');
@@ -16,20 +18,59 @@ app.engine('html', es6Renderer);
 app.set('view engine', 'html');
 app.set('views', 'views')
 
-// app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 app.use(session({
     store: new FileStore(),
     secret: process.env.SESSION_SECRET
 }));
 
+// async function demo() {
+//     const user = await User.getByEmail('childers@yahoo.com');
+//     user.setPassword("password");
+//     await user.save();
+//     console.log('you did the thing')
+// }
+// demo();
 
-app.get('/', (req, res) => {
-    res.send(`
-    <h1>Hello There!</h1>
-    `)
+
+app.get('/login', (req, res) => {
+    res.render('login', {
+        locals: {
+            email: '',
+            message: ''
+        }
+    });
 });
 
+app.post('/login', async (req, res) => {
+    console.log(req.body.email);
+    console.log(req.body.password);
 
+    const theUser = await User.getByEmail(req.body.email);
+    const correctPassword = theUser.checkPassword(req.body.password);
+    if (correctPassword) {
+        req.session.user = theUser.id;
+        req.session.save(() => {
+            res.redirect('/dashboard');
+        });
+    } else {
+        res.render('login', {
+            locals: {
+                email: req.body.email,
+                message: 'Password IS WRONG. GET OUT!'
+            }
+        });
+    }
+});
+
+app.get('/dashboard', (req, res) => {
+    if (req.session.user) {
+        console.log(`The user's id is: ${req.session.user}`);
+        res.send('Welcome to DevDepot!');
+    } else {
+        res.redirect('/login');
+    }
+});
 
 
 
