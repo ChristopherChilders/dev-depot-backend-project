b40eb9a39f36dfdc947debdce82986a8296f7a45
Christopher Childers <christopher.childers13@yahoo.com>
2019-04-10 11:05:29 -0400

    sets up the conn.js file, and puts some values into the database for us to manipulate

--- seed.sql
+++ seed.sql
@@ -1,19 +1,29 @@
 insert into users
 (first_name, last_name, email, username, password)
 values
-();
+('chris', 'childers', 'christopher.childers13@yahoo.com', 'ChristopherChilders', 'password'),
+('ryan', 'yim', 'email@email.com', 'Ryman', 'password'),
+('victor', 'troung', 'franken@stein.net', 'VDog', 'password'),
+('jason', 'boerner', 'jason@bourne.blackbriar', 'JB', 'password');
 
 INSERT into frameworks
 (name, code)
 values
-();
+('index.js', 'the path'),
+('schema.sql', 'the path'),
+('conn.js', 'the path');
 
 insert into favorites
 (user_id, framework_id, method_id)
 values
-();
+(1, 2, 2),
+(3, 1, 3),
+(2, 3, 3),
+(4, 2, 1);
 
 insert into methods
 (name, code)
 VALUES
-();
+('users model', 'the path'),
+('users controller', 'the path'),
+('users route', 'the path');
