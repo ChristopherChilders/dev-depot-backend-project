68ac1ccd805c716065884bd1ef0becc4fc37e56a
Jason Boerner <jboerner56@gamil.com>
2019-04-11 10:12:17 -0400

    add getById and get all users favorites to tests

--- tests/users.js
+++ tests/users.js
@@ -1,4 +1,5 @@
 const User = require('../models/users');
+const Favorites = require('../models/Favorites');
 const chai = require('chai');
 const expect = chai.expect;
 const chaiAsPromised = require('chai-as-promised');
@@ -9,5 +10,20 @@ describe('Users model', () => {
         const theUsers = await User.getAll();
         expect(theUsers).to.be.an.arrayOf(User);
     });
-    
+    it('should get one user by id', async () => {
+        const theUser = await User.getById(1);
+        expect(theUser).to.be.an.instanceOf(User);
+    });
+})
+
+describe('Users and Favorites', () => {
+    it('should get all the favorites for an individual user', async () => {
+        const theUser = await User.getById(1);
+        const theFavorites = await theUser.Favorites;
+        expect(theFavorites).to.be.an.instanceOf(Array);
+        expect(theFavorites).to.have.lengthOf(3);
+        for(let i=0; i<theFavorites.length; i++) {
+            expect(theFavorites[i]).to.be.an.instanceOf(Favorites);
+        }
+    })
 })
