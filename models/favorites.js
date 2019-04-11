a95509e3edb86a32a0806ae24eb84ed724bd8f28
Jason Boerner <jboerner56@gamil.com>
2019-04-11 07:52:24 -0400

    add getById to favorites class

--- models/favorites.js
+++ models/favorites.js
@@ -15,4 +15,19 @@ class Favorite {
             })
         })
     }
+    static getById (id) {
+        db.one(`select * from favorites where id=${id}`)
+        .then((favoriteData) => {
+            const favoriteInstance = new Favorite (
+                favoriteData.id,
+                favoriteData.user_id,
+                favoriteData.framework_id,
+                favoriteData.method_id
+            );
+            return favoriteInstance;
+        })
+        .catch(() => {
+            return null;
+        })
+    }
 }
