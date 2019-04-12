44658b23716885e4a06024602555cf9e77eb7309
victor truong <2truong93@gmail.com>
2019-04-12 14:43:11 -0400

    creates working template-display buttons for frameworks

--- controllers/frameworks.js
+++ controllers/frameworks.js
@@ -5,6 +5,17 @@ const highlight = require('pygments').colorize;
 
 
 async function goToFrameworks(req, res) {
+    data='template-here';
+    res.render('frameworks',{
+        locals: {
+            code:data
+        }
+    });
+
+};
+
+
+async function getUsersTemplate(req, res) {
 
     fs.readFile('templates/users.js', 'utf-8', (err, templatedata) => {
         // console.log(data);
@@ -15,10 +26,56 @@ async function goToFrameworks(req, res) {
                     code:data
                 }
             });
-        })
+        });
     });
-}
+};
+async function getModelsTemplate(req, res) {
+
+    fs.readFile('templates/models.js', 'utf-8', (err, templatedata) => {
+        // console.log(data);
+        //put res.render ino
+        highlight(templatedata,'js','html',(data)=>{
+            res.render('frameworks',{
+                locals: {
+                    code:data
+                }
+            });
+        });
+    });
+};
+async function getLoginTemplate(req, res) {
+
+    fs.readFile('templates/login.js', 'utf-8', (err, templatedata) => {
+        // console.log(data);
+        //put res.render ino
+        highlight(templatedata,'js','html',(data)=>{
+            res.render('frameworks',{
+                locals: {
+                    code:data
+                }
+            });
+        });
+    });
+};
+async function getControllerTemplate(req, res) {
+
+    fs.readFile('templates/controller.js', 'utf-8', (err, templatedata) => {
+        // console.log(data);
+        //put res.render ino
+        highlight(templatedata,'js','html',(data)=>{
+            res.render('frameworks',{
+                locals: {
+                    code:data
+                }
+            });
+        });
+    });
+};
 
 module.exports = {
-    goToFrameworks
+    goToFrameworks,
+    getUsersTemplate,
+    getModelsTemplate,
+    getLoginTemplate,
+    getControllerTemplate
 }
--- routes/frameworks.js
+++ routes/frameworks.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const Router = express.Router;
 const frameworksRouter = Router();
-const {goToFrameworks} = require('../controllers/frameworks');
+const { 
+    goToFrameworks,
+    getUsersTemplate,
+    getModelsTemplate,
+    getLoginTemplate,
+    getControllerTemplate
+} = require('../controllers/frameworks');
 
 frameworksRouter.get('/', goToFrameworks);
+frameworksRouter.get('/users', getUsersTemplate);
+frameworksRouter.get('/models', getModelsTemplate);
+frameworksRouter.get('/login', getLoginTemplate);
+frameworksRouter.get('/controller', getControllerTemplate);
 
 
 module.exports = frameworksRouter;
